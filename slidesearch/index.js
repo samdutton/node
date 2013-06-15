4c9ee20b779e7d001dca1061c5665515878bc8d7
samdutton <samdutton@gmail.com>
2013-06-15 18:23:43 +0100

    Working: search

---
+++ slidesearch/index.js
@@ -0,0 +1,189 @@
+var nano = require('nano')('http://localhost:5984');
+var jsdom = require('jsdom');
+var request = require('request');
+var localCouchName = 'presentations';
+var remoteCouchName = 'https://chrome.iriscouch.com:6984/presentations';
+var presentations;
+var presentationURLs;
+
+nano.db.destroy(localCouchName, function() {
+  nano.db.create(localCouchName, function() {
+    presentations = nano.use(localCouchName);
+    getPresentationURLs();
+  });
+});
+
+function insertOrUpdateDoc(db, doc, key, isInsertAttempted) {
+  db.insert(doc, function (error, body, header) {
+    if (error) {
+      if (error.error === 'conflict' && !isInsertAttempted) {
+        return db.get(key, function(error, doc) {
+          doc._rev = doc._rev;
+          insertOrUpdateDoc(db, doc, key, true);
+        });
+      } else {
+        console.log('>>> insertOrUpdateDoc() error:', error);
+      }
+    } else {
+//      console.log('Inserted ', doc.url);
+        nano.db.get(localCouchName, function(error, body) {
+        if (error) {
+          console.log('nano.db.get error:', error);
+        } else {
+          if (body.doc_count === presentationURLs.length) {
+            // maybe add timeout to replicate regardless
+            // in case of XHR problems getting presentations
+            console.log('nano.db.get successful:', body.doc_count);
+
+            nano.db.replicate(localCouchName, remoteCouchName, {create_target:true},
+              function(error, body) {
+                if (error) {
+                console.log('>>> nano.db.replicate() error:', error);
+                } else {
+                  console.log('nano.db.replicate() success', body);
+                }
+            });
+          }
+        }
+      });
+    }
+  });
+}
+
+function insertOrUpdatePresentation(presentation){
+  insertOrUpdateDoc(presentations, presentation, presentation.url, false);
+}
+
+function handleURLs(error, window, url) {
+  var presentation = {
+    slides: [],
+    url: url
+  };
+  var d =  window.document;
+  var slides = [];
+}
+
+// spreadsheet listing URLs for presentations
+var SPREADSHEET_URL = 'https://docs.google.com/spreadsheet/pub?key=0AtxlQg9XpugsdGVUTi1QbUdYbnNTd0tzUHlFZGRaUnc&single=true&gid=0&output=csv';
+
+function getPresentationURLs(){
+  request(
+    {uri: SPREADSHEET_URL},
+    function(error, response, body){
+      if (error) {
+        console.log('>>> Error getting presentations URLs', error)
+      } else if (response.statusCode == 200) {
+        presentationURLs = body.split('\n');
+        for (var i = 0; i != presentationURLs.length; ++i) {
+          requestPresentation(presentationURLs[i]);
+        }
+      }
+    }
+  );
+}
+
+function requestPresentation(url){
+  request({uri: url}, function(error, response, body){
+    if (error) {
+      console.log('>>> Error getting presentation ' + url + ":", error);
+      var index = array.indexOf(url);
+      presentationURLs.splice(index, 1); // to maintain list of viable URLs
+    } else if (response.statusCode == 200) {
+      jsdom.env({
+        html: body,
+        // scripts: [
+        //   'http://localhost:8888/node/request/js/lib/jquery-2.0.0.min.js'
+        // ]
+      }, function(error, window){handlePresentation(error, window, url)});
+    }
+
+  });
+}
+
+function addSlideData(presentation, slide, slideElement){
+  slide.text = getText(slideElement);
+
+  var h1 = slideElement.querySelector('h1');
+  if (h1 && h1.textContent.trim() !== ''){
+    slide.heading = getText(h1);
+  }
+  var h2 = slideElement.querySelector('h2');
+  if (h2 && h2.textContent.trim() !== ''){
+    slide.heading = getText(h2);
+  }
+  var article = slideElement.querySelector('article');
+  if (article && article.textContent.trim() !== ''){
+    slide.article = getText(article);
+  }
+  var aside = slideElement.querySelector('aside');
+  if (aside && aside.textContent.trim() !== ''){
+    slide.aside = getText(aside);
+  }
+
+  var images = slideElement.querySelectorAll('img');
+  if (images.length){
+    slide.images = [];
+    for (var i = 0; i != images.length; ++i){
+      var image = images[i];
+      var alt = image.alt || image.title;
+      if (alt && alt.trim() != ''){
+        slide.text += ' ' + image.alt;
+        slide.images.push({alt: alt, src: image.src});
+      }
+    }
+    if (slide.images.length === 0){
+      // remove empty image array caused when no images on the slide have alt attributes
+      delete slide.images;
+    }
+  }
+  presentation.slides.push(slide);
+}
+
+function handlePresentation(error, window, url) {
+  var presentation = {
+    slides: [],
+    url: url
+  };
+
+  // cope with different document formats
+  var d =  window.document;
+  var slideElements;
+  if (d.querySelectorAll('slide').length > 0){
+    slideElements = d.querySelectorAll('slide');
+  } else if (d.querySelectorAll('article').length > 0){
+    slideElements = d.querySelectorAll('article');
+  } else if (d.querySelectorAll('section').length > 0){
+    slideElements = d.querySelectorAll('section');
+  } else {
+    console.log('Could not find element name for slides for ', url);
+    return;
+  }
+
+  for (var i = 0; i !== slideElements.length; ++i){
+    var slide = {
+      slideNumber: i + 1
+    };
+    addSlideData(presentation, slide, slideElements[i]);
+  } // each slideElement
+
+  // newer presentations have title inserted dynamically with JavaScript
+  var h1 = d.querySelector('h1');
+  if (window.document.title){
+    presentation.title = window.document.title;
+  } else if (h1 && h1.textContent.trim() !== ''){
+    presentation.title = getText(h1);
+  } else { // last resort
+    presentation.title = url.split('\/\/')[1];
+  }
+
+//  console.log('Inserting presentation: ', presentation.url, presentation.slides.length);
+  insertOrUpdatePresentation(presentation)
+
+//  console.log(presentation.url + ',' + presentation.title + ',(' + presentation.slides.length +' slides)');
+
+}
+
+
+function getText(element){
+  return element.textContent.trim().replace(/\s+/g, ' ');
+}
--- slidesearch/js/main.js
+++ slidesearch/js/main.js
@@ -1,264 +1,91 @@
-var isReady;
-var MINTRACKS = 250; // !!!hack to determine if getting/parsing data is complete
-var numTracks = 0;
-var trackPath = "tracks/";
-var trackSuffix = ".srt";
-var resultsDiv = $("#results");
-var query;
-var $query = $("#query");
-var $numResults = $("#numResults");
+var pouchdb;
+var localCouchName = 'http://127.0.0.1:5984/presentations';
+var pouchName = 'idb://presentations';
+var remoteCouchName = 'https://chrome.iriscouch.com:6984/presentations';
+// var from = 'idb://bar';
 
-getVideoData()
-
-// do query if URL has query string
-var href = document.location.href;
-var q = href.split("?q=")[1];
-if (q){
-	query = q.replace(/[^a-zA-Z]/g, " ");
-	$query.css('color', '#ddd');
-	$query.val(query);
-	var interval = setInterval(function(){
-		if (isReady){
-		$query.css('color', '#444');
-			$numResults.html("Searching...");
-			getResults(query);
-			clearInterval(interval);
-		}
-	}, 1000);
-}
-
-function getVideoData() {
-	// the keys for the videos object, defined in videos.js, are YouTube IDs
-	for (var id in videos) {
-		getCueData(id); // get cue data from .srt files in tracks folder
-		getYouTubeData(id); // update video data from the YouTube data API
-	}
-}
-
-// function handleVideosComplete(){
-// 	console.log("finished! ", videos);
-// }
-
-// video data will also be updated from YouTube data API
-function getCueData(videoId){
-	videos[videoId].cues = [];
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", trackPath + videoId + trackSuffix);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState === 4 && xhr.status === 200) {
-	  	var track = xhr.responseText;
-	  	var lines = track.match(/^.*((\r\n|\n|\r)|$)/gm);
-	  	var cues = videos[videoId].cues;
-	  	var currentCue = {"text": "", "videoId": videoId};
-	  	for (var i = 0; i != lines.length; ++i){
-	  		var line = lines[i];
-	  		// if line is just a cue ID (i.e. just digits)
-	  		if (line.match(/^\d+\s*$/)){
-	  			if (currentCue.text) {
-	  				// get rid of redundant whitespace after combining lines
-	  				currentCue.text.trim();
-	  				cues.push(currentCue);
-				  	currentCue = {"text": "", "videoId": videoId}; // remove text!
-	  			}
-	  		} else if (line.match(/^\s*$/)){ // line is empty
-	  			continue;
-	  		} else { // line is timings or text
-	  			var timings = line.match(/\d\d:\d\d:\d\d.\d\d\d/g);
-	  		  if (!timings){ // line is text
-	  		  	// replace return with a space: cues may be split between two lines
-						currentCue.text += line.replace(/\n/, " ");
-					} else if (timings.length === 2) { // line is timing
-	  				currentCue.startTime = toDecimalSeconds(timings[0]);
-					}
-	  		}
-	  	}
-	  	numTracks += 1;
-  		if (numTracks === MINTRACKS) {
-  			isReady = true;
-  			var div = document.querySelector('div#queryExplanation');
-  			div.style.color = '#ccc';
-  			div.innerText =
-  				'Enter text to search transcripts, then click on a result to view video.';
-  			var input = document.querySelector('input#query');
-  			input.disabled = false;
-  			input.focus();
-  		}
-
-  	} // xhr.readyState === 4 && xhr.status === 200
-  } // xhr.onreadystatechange
-	xhr.send();
-}
-
-// update video data from YouTube data API
-function getYouTubeData(videoId){
-	try {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://gdata.youtube.com/feeds/api/videos/" + videoId + "?alt=json");
-		xhr.onreadystatechange = function() {
-		  if (xhr.readyState === 4 && xhr.status === 200) {
-		    var obj = JSON.parse(xhr.responseText);
-		  	videos[videoId].viewCount = obj.entry.yt$statistics.viewCount;
-		  	if (obj.entry.gd$rating && obj.entry.gd$rating.average){
-			    videos[videoId].rating = obj.entry.gd$rating.average;
-			  } else {
-			  	videos[videoId].rating = '';
-			  }
-	  	}
-	  }
-		xhr.send();
-	} catch(e){
-		console.log(e);
-	}
-}
-
-
-
-// http://storage.googleapis.com/io2012/headshots/mkwst.jpg
-
-var youTubePlayer = document.querySelector(".youtube-player");
-// toggle display of cue or query results
-function addClickHandler(cueDiv, cue) {
-  cueDiv.click(function() {
-  	// don't reload video if the clicked cue is for current video
-		if (youTubePlayer.src.indexOf(cue.videoId) != -1){
-			callPlayer("youTubePlayer", "seekTo", [cue.startTime]);
-		} else {
-			youTubePlayer.src =
-				"http://www.youtube.com/embed/" + cue.videoId +
-				"?start=" + cue.startTime +
-				"&autoplay=1&enablejsapi=1"
-		}
-	});
-}
-
-function displayResults(results) { // results is an array of cues
-	document.querySelector("*").style.cursor = "";
-	resultsDiv.empty();
-  $("#numResults").empty();
-	if ($query.val().length < 2) {
-    return false;
-  }
-
-	var numResults = results.length;
-	var currentVideoId, videoDiv, cuesDiv;
-	var i;
-	// enable searching for two letter combinations (for example)
-	// but don't show results when there are too many matches
-	if (numResults > 5000){
-		$numResults.html(numResults + " results (too many to display)");
-		return;
-	} else {
-		$numResults.html(numResults + " result(s)");
-	}
-  for (i = 0; i !== numResults; ++i) {
-    var cue = results[i];
-		// for each video (i.e. new currentVideoId)
-		// create divs and add the video title,
-		// then add a click handler to display video
-		if (!currentVideoId || currentVideoId !== cue.videoId) {
-			currentVideoId = cue.videoId;
-			var video = videos[currentVideoId];
-			videoDiv = $("<div class='video' />");
-
-			var detailsDiv = $("<details class='videoDetails' />");
-			detailsDiv.append("<summary class='videoTitle' title='Click to view video information'>" +
-					video.title + "</summary>");
-			detailsDiv.append("<img class='videoThumbnail' src='http://img.youtube.com/vi/" +
-				currentVideoId + "/hqdefault.jpg' title='Default thumbnail image' />");
-			if (!!video.summary){
-				detailsDiv.append("<div class='videoSummary'>" + video.summary + "</div>");
-			}
-			detailsDiv.append("<div class='videoRating'><strong>Rating: </strong>" +
-				video.rating + "</div>");
-			detailsDiv.append("<div class='videoViewCount'><strong>View count: </strong>" +
-				video.viewCount + "</div>");
-			videoDiv.append(detailsDiv);
-
-			if (video.speakers && video.speakers.length !== 0){
-				videoDiv.append("<div class='speakers'>" + video.speakers.join(", ") + "</div>");
-			}
-
-			cuesDiv = $("<div class='cues' title='Click to play video at this point' />");
-			videoDiv.append(cuesDiv);
-			resultsDiv.append(videoDiv);
-		}
-
-		var cueStartTimeHTML = "<span class='cueStartTime'>" +
-			toHoursMinutesSeconds(cue.startTime) + ": </span>";
-		var cueTextHTML = cue.text.replace(new RegExp("(" + query +
-			")", "gi"), "<em>$1</em>"); // empasise query
-		cueTextHTML = "<span class='cueText'>" + cueTextHTML + "</span>";
-		// add cue to div.cues
-		var cueDiv =
-			$("<div class='cue'>" +
-			cueStartTimeHTML +
-			cueTextHTML +
-			"</div>");
-		addClickHandler(cueDiv, cue);
-		cuesDiv.append(cueDiv);
-  }
-}
-
-function getResults(query){
-	document.querySelector("*").style.cursor = "wait";
-	$numResults.html("Searching...");
-	var cues = [];
-	for (id in videos){
-		var video = videos[id];
-		for (var i = 0; i != video.cues.length; ++i) {
-			var cue = video.cues[i];
-			var re = new RegExp(query, "i");
-			if (re.test(cue.text)) {
-				cues.push(cue);
-			}
-		}
-	}
-	displayResults(cues);
-}
-
-$query.bind('input', function() {
-	resultsDiv.empty();
-	query = $(this).val();
-  if (query.length < 2) {
-  	$numResults.empty();
-    return false;
-  }
-  // add 300ms delay between getting keypresses
-	if(typeof(window.inputTimeout) != "undefined"){
-		window.clearTimeout(inputTimeout);
-	}
-	window.inputTimeout = window.setTimeout(function() {
-		getResults(query);
-	}, 300);
-});
-
-function elapsedTimer(message) {
-    if (elapsedTimer.isStarted) {
-        console.log(message, (Date.now() - elapsedTimer.startTime));
-        elapsedTimer.startTime = Date.now();
+function init(from, to){
+  Pouch(to, function(error, db){
+    pouchdb = db;
+    if (error) {
+      console.log("Pouch error creating database:", error);
     } else {
-        elapsedTimer.startTime = Date.now();
-        elapsedTimer.isStarted = true;
+      // Pouch.replicate(from, to, {continuous: true}, function (error, changes) {
+      //   if (error) {
+      //     console.log('Pouch replicate() error: ', error);
+      //   } else {
+      //     db.allDocs({include_docs: true}, function(error, docs) {
+      //       console.log('Pouch replicated ' + doc.rows.length + ' rows: ', docs.rows);
+      //     });
+      //   }
+      // });
     }
+  });
 }
 
-// srt format is 01:01:58,310 (it's French, hence the comma)
-// hours:minutes:seconds,milliseconds
-function toDecimalSeconds(srtTime) {
-	var split = srtTime.match(/\d{2}/g);
-	// hours, minutes, seconds -- ignore milliseconds
-	return split[0] * 3600 + split[1] * 60 + split[2] * 1; // must convert from string
+function destroy(name){
+  Pouch.destroy(name, function(error){
+    if (error) {
+      console.log("Pouch error destroying database:", error)
+    } else {
+      console.log('Pouch destroyed database:', name);
+    }
+  });
 }
 
-// Convert decimal time to hh:mm:ss
-// e.g. convert 123.3 to 2:03
-function toHoursMinutesSeconds(decimalSeconds){
-	var hours = Math.floor(decimalSeconds/3600);
-	var mins = Math.floor((decimalSeconds - hours * 3600)/60);
-	var secs = Math.floor(decimalSeconds % 60);
-	if (secs < 10) {
-		secs = "0" + secs
-	};
-	return hours + ":" + mins + ":" + secs;
+// destroy(remoteCouchName);
+// destroy(pouch);
+var pouchdb;
+
+init(remoteCouchName, pouchName);
+
+// var getAllButton = document.querySelector('button#getAll');
+
+// getAllButton.onclick = function(){
+//   pouchdb.allDocs({include_docs: true}, function(error, docs) {
+//     console.log('docs: ', docs);
+//     console.log('Pouch replicated ' + docs.rows.length + 'rows: ', docs.rows);
+//   });
+// };
+
+function map(presentation){
+  var slides = presentation.slides;
+  for (var i = 0; i !== slides.length; ++i) {
+    var slide = slides[i];
+    var regExp = new RegExp(query, "i");
+//    console.log(window.query, slide.text);
+    // if (regExp.test(slide.text)) {
+    //   console.log(presentation, slide, i + 1);
+    // }
+    if (slide.text.indexOf(queryString.toLowerCase()) != -1) {
+      log(slide.text);
+//      console.log(presentation, slide, i + 1);
+    }
+  }
 }
 
+function get(string){
+  queryString = string;
+  pouchdb.query({map: map}, {reduce: false},
+    function(error, response) {
+      if (error){
+        // alert('pouchdb.query error: ', error);
+        console.log('pouchdb.query error: ', error);
+      } else {
+        // alert('pouchdb.query success: ', response);
+//        console.log('pouchdb.query success: ', response);
+      }
+    }
+  );
+}
+
+var searchButton = document.querySelector('button#search');
+var queryInput = document.querySelector('input#query');
+searchButton.onclick = function(){
+  var queryString = queryInput.value.replace(/[^a-zA-Z]/g, " ");
+  get(queryString);
+};
+
+function log(string){
+  document.getElementById('data').innerHTML = string;
+}
